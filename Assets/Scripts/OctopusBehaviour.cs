8e2efc0f55efe2a3170f67d463581d7287486df1
wierzejs <wierzejs@uni.coventry.ac.uk>
2017-04-12 15:41:15 +0100

    Adding the game to GitHub

---
+++ Assets/Scripts/OctopusBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OctopusBehaviour : MonoBehaviour {
+    public float moveSpeed = 3f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * moveSpeed);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed = 5f;
+
+
+    private int score = 0;
+
+
+
+	void Update () {
+	if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(1, 0, 0)*Time.deltaTime * moveSpeed);
+        }
+    if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-1, 0, 0) * Time.deltaTime * moveSpeed);
+        }
+    if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime * moveSpeed);
+        }
+    if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * moveSpeed);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("OnTriggerEneter2D" + other.gameObject.name);
+        if (other.gameObject.name == "plasticbag")
+        {
+            Destroy(other.gameObject);
+            Debug.Log("Plastic bag collected");
+            score += 1;
+        }
+    }
+    
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 30), "Score: " + score);
+    }
+    
+}
